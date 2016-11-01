ddd54c0ade349d677ed2cc1f51c5f0d55a09287e
Philipp Arndt <2f.mail@gmx.de>
2016-11-01 13:02:08 +0100

    Simplified the example

--- de.rnd7.e4.test.addressbar/src/de/rnd7/e4/test/addressbar/MyEditor.java
+++ de.rnd7.e4.test.addressbar/src/de/rnd7/e4/test/addressbar/MyEditor.java
@@ -6,8 +6,12 @@ import javax.inject.Inject;
 import org.eclipse.e4.ui.di.Persist;
 import org.eclipse.e4.ui.model.application.ui.MDirtyable;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 public class MyEditor {
@@ -16,11 +20,35 @@ public class MyEditor {
 
 	@Inject
 	public MyEditor(final Composite parent, final MDirtyable dirtyable) {
-		this.text = new Text(parent, SWT.BORDER | SWT.MULTI);
-		this.text.setLayoutData(new GridData(GridData.FILL));
+		final GridLayout layout = new GridLayout();
+		layout.marginLeft = 0;
+		layout.marginRight = 0;
+		layout.marginTop = 0;
+		layout.marginBottom = 0;
+		layout.horizontalSpacing = 0;
+		layout.verticalSpacing = 5;
+		parent.setLayout(layout);
+		parent.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_CYAN));
+		final Label label = new Label(parent, SWT.NONE);
+		label.setText("This is a shared header");
+
+		final CTabFolder folder = new CTabFolder(parent, SWT.BOTTOM);
+		folder.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		final CTabItem a = new CTabItem(folder, SWT.NONE);
+		a.setText("a");
+
+		final CTabItem b = new CTabItem(folder, SWT.NONE);
+		b.setText("b");
+
+		this.text = new Text(folder, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		this.text.setLayoutData(new GridData(GridData.FILL_BOTH));
 		this.text.addModifyListener(e -> {
 			dirtyable.setDirty(true);
 		});
+
+		a.setControl(this.text);
+		folder.setSelection(0);
 	}
 
 	@Persist
--- de.rnd7.e4.test.addressbar/src/de/rnd7/e4/test/addressbar/MyView.java
+++ de.rnd7.e4.test.addressbar/src/de/rnd7/e4/test/addressbar/MyView.java
@@ -6,7 +6,6 @@ import javax.inject.Inject;
 
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
@@ -31,23 +30,16 @@ public class MyView {
 		button.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(final SelectionEvent e) {
-				final MPart part = partService.createPart("de.rnd7.e4.test.addressbar.partdescriptor.myeditor");
+
+				final MPart part = partService.showPart("de.rnd7.e4.test.addressbar.partdescriptor.myeditor",
+						PartState.CREATE);
 				part.setLabel("New Dynamic Editor");
-
-				final MPartStack stack = (MPartStack) modelService.find("de.rnd7.eclipse4.test.partstack.editor",
-						application);
-
-				if (stack != null && stack.isVisible()) {
-					stack.getChildren().add(part);
-					stack.setSelectedElement(part);
-				} else {
-					partService.showPart(part, PartState.ACTIVATE);
-				}
 
 				final MyEditor editor = (MyEditor) part.getObject();
 				if (editor != null) {
 					editor.setInput(UUID.randomUUID().toString());
 				}
+
 			}
 		});
 
