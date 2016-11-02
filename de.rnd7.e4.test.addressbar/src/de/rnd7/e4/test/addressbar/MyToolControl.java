6f06362256cb370641fc2567907f857b8fcef9aa
Philipp Arndt <2f.mail@gmx.de>
2016-11-02 15:10:56 +0100

    Update MyToolControl.java

--- de.rnd7.e4.test.addressbar/src/de/rnd7/e4/test/addressbar/MyToolControl.java
+++ de.rnd7.e4.test.addressbar/src/de/rnd7/e4/test/addressbar/MyToolControl.java
@@ -10,10 +10,18 @@ import org.eclipse.swt.widgets.Text;
 public class MyToolControl {
 	@Inject
 	public MyToolControl(final Composite parent) {
-		final Composite composite = new Composite(parent, SWT.NONE);
+		Composite composite = new Composite(parent, SWT.NONE) {
+			@Override
+			public Point computeSize(int wHint, int hHint) {
+				final Point size = super.computeSize(wHint, hHint);
+				
+				return new Point(100, size.y);
+			}
+		};
 		composite.setLayout(new GridLayout());
 		final Text text = new Text(composite, SWT.BORDER);
 
 		text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
 	}
 }
